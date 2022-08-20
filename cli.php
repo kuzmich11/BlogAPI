632e92df7a268ce5438a715620a19c26191cf32d
Vladimir Kuznetsov <v.a.kuznetsov-83@yandex.ru>
2022-08-20 23:10:26 +0300

    Папка vendor внесена в gitignore

--- cli.php
+++ cli.php
@@ -15,23 +15,26 @@ use KuznetsovVladimir\BlogApi\Blog\Comment;
 //});
 
 $faker = Faker\Factory::create('ru_RU');
+if (isset($argv[1])) {
 
-if ($argv[1] === 'user') {
-    $user = new User(1, $faker->lastName(), $faker->firstName());
-    echo $user;
-}
-if ($argv[1] === 'post') {
-    $post = new Post(1,
-        new User(1, $faker->firstName(), $faker->lastName()), $faker->sentence(3), $faker->paragraphs(3, true));
-    echo $post;
-}
-if ($argv[1] === 'comment') {
-    $comment = new Comment(1,
-        $user = new User (1, $faker->lastName(), $faker->firstName()),
-        new Post(1, $user, $faker->sentence(3), $faker->paragraphs(3, true)),
-        $faker->paragraphs(1, true)
-    );
-    echo $comment;
+
+    if ($argv[1] === 'user') {
+        $user = new User(1, $faker->lastName(), $faker->firstName());
+        echo $user;
+    }
+    if ($argv[1] === 'post') {
+        $post = new Post(1,
+            new User(1, $faker->firstName(), $faker->lastName()), $faker->sentence(3), $faker->paragraphs(3, true));
+        echo $post;
+    }
+    if ($argv[1] === 'comment') {
+        $comment = new Comment(1,
+            $user = new User (1, $faker->lastName(), $faker->firstName()),
+            new Post(1, $user, $faker->sentence(3), $faker->paragraphs(3, true)),
+            $faker->paragraphs(1, true)
+        );
+        echo $comment;
+    }
 }
 //$post = new Post(1, new User(1, 'Иван', 'Никитин'), "Заголовок", "Текст статьи"
 //);
